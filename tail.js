840425147c922c95174dc1de3f48bf9aa7c19267
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-02-06 18:35:00 -0800

    tail function captures tail elements in an array

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}.`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const tail = function(array) {
+  return console.log(array.slice(1));
+};
+
+
+
+/* test cases:
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+assertEqual(head([5,6,7]), 5);
+
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); */
