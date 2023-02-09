c97fac8bcbe17f2a290580436976ea4686c00979
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-02-08 16:48:13 -0800

    letter positions function

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const eqArrays = function(a, b) {
+  console.log("this is a:", a);
+  console.log("this is b:", b);
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    let result = false;
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}.`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// test cases:
+// const sentence = "hello";
+// console.log(letterPositions(sentence));
