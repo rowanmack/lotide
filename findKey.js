b1d999a3dea9797b3b4966bc2e858509844439a1
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-02-09 18:08:02 -0800

    find key function

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}.`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = (object, callback) => {
+  for (let element in object) {
+    if (callback(object[element])) {
+      return element
+    }
+  }
+};
+
+const results = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(results, "noma");
