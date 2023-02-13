eb6acb8d2ad2f5e21a57e2617665d618309e5706
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-02-13 15:22:24 -0800

    made formatting adjustements

--- test/headTest.js
+++ test/headTest.js
@@ -2,5 +2,5 @@ const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 console.log(head([3, 2, 6]));
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- test/tailTest.js
+++ test/tailTest.js
@@ -4,5 +4,5 @@ const tail = require('../tail');
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail([5,6,7]), [6, 7]);
+assertEqual(tail([5, 6, 7]), [6, 7]);
 assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
